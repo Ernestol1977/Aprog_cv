39928bf7ea534641b2bed2c968a881cc7bac5fc3
ernestol1977 <ernestolondero@gmail.com>
2023-02-28 21:53:12 -0300

    CV - corrección sin formulario

---
+++ buttom.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 250) {
+        document.querySelector('.gotop-container').classList.add('show');
+    } else {
+        document.querySelector('.gotop-container').classList.remove('show');
+    }
+};
+
+document.querySelector('.gotop-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
